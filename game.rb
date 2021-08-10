c2ee5b7c6e4b5890c18e42e87053ee72d3835de6
Neil <neil.andrewartha@gmail.com>
2021-08-10 15:13:06 +0800

    Setup board and win classes

--- game.rb
+++ game.rb
@@ -1,11 +1,21 @@
 class Board
   attr_accessor :board
   def initialize
-    @board = ["", "", "", "", "", "", "", "", ""]
+    @board = ["", "", "", 
+              "", "", "", 
+              "", "", ""]
   end
 end
 
 class Win
-  @win = [0,1,2]  
+  @win =[ [0,1,2], 
+          [0,3,6], 
+          [0,4,8], 
+          [1,4,7],
+          [2,5,8], 
+          [2,4,6], 
+          [3,4,5], 
+          [6,7,8]
+        ]  
 end
 
