5f89d3bec7a33946e787760e60dc61242293cc87
Neil <neil.andrewartha@gmail.com>
2021-08-12 15:20:06 +0800

    Sepeted code into rb files by topic

---
+++ players.rb
@@ -0,0 +1,17 @@
+class Player
+  attr_reader :name
+  attr_accessor :symbol
+
+  def initialize
+    @name = name
+    @symbol = symbol
+  end
+end
+
+class ComputerPlayer
+  attr_accessor :symbol
+  def initialize
+    @name = 'computer'
+    @symbol = @symbol
+  end
+end
