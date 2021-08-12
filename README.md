bc4ef8b91f8e162808920b1f7cd9e78d24f6e03e
Neil <neil.andrewartha@gmail.com>
2021-08-12 14:35:12 +0800

    Added board class with display fucntion & player class

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # tic-tac-toe
 
 This is an Odin Project to creat a tic tac toe game using Ruby that runs in terminal
+
+How the game works
+A 3x3 grid is display 
+ >>Need a board class that consist of an array consisting of 9 spaces
+ >>Need a display function that takes the current state of the board array and displays it in a 3x3 grid
+ >> Grid spaces are numbered 1-9
+
+Two players take it in turns to select an avaiable space
+  >> Player one is always a person
+  >> Need to prompt for the number of human players
+  >> If 1 human set player 2 as a computer player
+  >> Need a class for each player that holds their name and their symbol
+  >> Player two can be either a computer or person
+  >> Randomise who goes first
+  >> First to go is assigned the X symbol
+  >> Second to go is assigned the O symbol
+
+Grid selection by human
+  >> Prompt player to enter which square they want to select by entering its resepctive number
+  >> 
+
+
+  
+  >> First player selects an avaiable square
+  >> Square changes to the X sybmol
+  >> Second player picks an available square
+  >> Sqaure changes to the O sybmol
+  Player cannot select a space already ocupied by the other player
+
+A player wins by owning 3 spaces that form a line, inclduing diagonals  
--- game.rb
+++ game.rb
@@ -1,31 +1,50 @@
 class Board
   attr_accessor :board
+
   def initialize
-    @board = ["", "", "", 
-              "", "", "", 
-              "", "", ""]
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  def display_board
+    puts  " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts  '-----------'
+    puts  " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts  '-----------'
+    puts  " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 end
 
-class Win
-  @win =[ [0,1,2], 
-          [0,3,6], 
-          [0,4,8], 
-          [1,4,7],
-          [2,5,8], 
-          [2,4,6], 
-          [3,4,5], 
-          [6,7,8]
-        ] 
+def win
+  @win = [[0, 1, 2],
+          [0, 3, 6],
+          [0, 4, 8],
+          [1, 4, 7],
+          [2, 5, 8],
+          [2, 4, 6],
+          [3, 4, 5],
+          [6, 7, 8]]
 end
 
-class DisplayBoard
+class Player
+  attr_reader :name
+  attr_accessor :symbol
+
   def initialize
-    puts  " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts  "-----------"
-    puts  " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts  "-----------"
-    puts  " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-   end
+    puts 'Please enter your name'
+    @name = gets.chomp
+    @symbol = ''
+  end
 end
-  
+
+class ComputerPlayer
+  attr_accessor :symbol
+
+  def initialize
+    @symbol = ''
+  end
+end
+
+game = Board.new
+game.display_board
+me = PlayerOne.new
+puts "Hi #{me.name}!"
