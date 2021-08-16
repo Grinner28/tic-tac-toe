be1322545ca1cec1e10f0caa72bdd3ae9261a5fd
Neil <neil.andrewartha@gmail.com>
2021-08-16 15:19:42 +0800

    Setup messages file with messaging method modual

--- gamesetup.rb
+++ gamesetup.rb
@@ -1,2 +1,24 @@
 require_relative 'board'
 require_relative 'players'
+require_relative 'messages'
+
+puts display_intro()
+
+puts display_how_many_players()
+
+# Inital setup
+# Display a welcome message
+# Ask if game will be 1 or 2 human players?
+# If 2 players
+#    Prompt please enter player 1s name and create player with said name
+#    Prompt please enter player 2s name and create player with said name
+ 
+# If 1 human player
+#   Prompt please enter your name and create player with said name
+#    Create computer player
+
+# Assigning first and second player
+#    Randomly select who will go first
+#    Assign first player the symbol X
+#    Assign second player the symbol O
+#    Set first as current player
---
+++ messages.rb
@@ -0,0 +1,34 @@
+module Messages
+  def display_intro
+    puts "Lets play tic-tac-toe"
+  end
+
+  def display_how_many_players
+    "How many human players?"
+  end
+
+  def display_select
+    "Please select a available space"
+  end
+
+  def display_select_error
+    "That space is already taken. Please select an available splace"
+  end
+
+  def display_win
+    "Well done, you win"
+  end
+
+  def display_lose
+    "Sorry you lose. Better luck next time"
+  end
+
+  def display_tie
+    "The match is a tie"
+  end
+
+  def display_replay
+    "Would you like to play again"
+  end
+
+end
